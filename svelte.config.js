f0fb92ba08d3250472a63b47817733cfde47a6ec
Sup#2.0 <sup2point0@gmail.com>
2025-05-20 14:18:33 +0100

    dev: configure katex

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,12 @@ import adapter from "@sveltejs/adapter-static";
 import { sveltePreprocess } from "svelte-preprocess";
 
 import { mdsvex } from "mdsvex";
+import remarkFootnotes from "remark-footnotes";
+import remarkIndexFootnotes from "remark-numbered-footnote-labels";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex-svelte";
+
+import { correct_hast_tree, render_katex_blocks } from "./latex-config.js";
 
 
 const config = {
@@ -32,6 +38,16 @@ const config = {
   preprocess: [
     mdsvex({
       extensions: [".md", ".svx"],
+      remarkPlugins: [
+        remarkFootnotes,
+        remarkIndexFootnotes,
+        remarkMath,
+        render_katex_blocks
+      ],
+      rehypePlugins: [
+        correct_hast_tree,
+        rehypeKatex,
+      ],
     }),
     sveltePreprocess({
       scss: {
