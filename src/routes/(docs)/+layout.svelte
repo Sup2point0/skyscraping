5ce78ffa7860687a1d93324882d119c4bb633c58
Sup#2.0 <sup2point0@gmail.com>
2025-05-21 12:30:28 +0100

    dev: restructure layout so scrolling is on `html` instead of `main`

--- src/routes/(docs)/+layout.svelte
+++ src/routes/(docs)/+layout.svelte
@@ -31,9 +31,11 @@ function check_mobile() {
 </script>
 
 
-<div class="layout">
+<div class="layout" class:mobile>
   {#if !mobile}
-    <NavPane />
+    <div class="nav-pane">
+      <NavPane />
+    </div>
   {/if}
 
   <main>
@@ -52,21 +54,30 @@ function check_mobile() {
 
 <style lang="scss">
 
-$mobile-width: 50rem;
-
 .layout {
   width: 100%;
-  height: 100vh;
-  overflow: hidden;
+  position: relative;
   display: flex;
   flex-flow: row nowrap;
   justify-content: stretch;
 }
 
+.nav-pane {
+  max-height: 100vh;
+  overflow-y: auto;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  align-self: flex-start;
+}
+
 main {
   flex: 20 0;
   padding: 2rem 3rem;
-  overflow-y: auto;
+}
+
+.layout.mobile main {
+  padding: 2rem 8vw;
 }
 
 </style>
