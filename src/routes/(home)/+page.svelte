c3cadc725b30aafdebee3b993fbc3c4b15acea59
Sup#2.0 <sup2point0@gmail.com>
2025-06-10 17:34:26 +0100

    fix: repair temporary link on homepage

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,6 +1,12 @@
+<script>
+
+import { base } from "$app/paths";
+
+</script>
+
 <p>
   Under construction!
 </p>
 <p>
-  Navigate to <a href="/walk/foreword">foreword</a> for now.
+  Navigate to <a href="{base}/walk/foreword">foreword</a> for now.
 </p>
