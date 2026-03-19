cc304e51cddfe2584785c5e8f3a78be111b06af8
Sup#2.0 <sup2point0@gmail.com>
2026-03-19 22:31:51 +0000

    dev: add `<aside>` styling

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    open: process.env.BROWSER = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
+  },
 });
